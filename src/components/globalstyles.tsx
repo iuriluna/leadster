a22b6a16c55f057fd329c44b790f6973e2d9acfc
iuriluna <iurideluna@gmail.com>
2023-06-24 18:21:53 -0300

    style: fonte plus jakarta adicionada

--- src/components/globalstyles.tsx
+++ src/components/globalstyles.tsx
@@ -1,3 +1,8 @@
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap');
+</style>
+
+
 import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
@@ -6,8 +11,7 @@ const GlobalStyle = createGlobalStyle`
     color: ${({ theme }) => theme.colors.primary};
     padding: 0;
     margin: 0;
-    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
-      Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+    font-family: 'Plus Jakarta Sans', sans-serif;
   }
 
   a {
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,20 +15,20 @@ import LogoImage from '../assets/logo.png'
 export default function Home() {
   return (
     <>
-      <Header>
-        <Image 
-            src={LogoImage} 
-            width='194px'
-            height='42px'
-            alt='logo leadster' 
-        />
-        <Branding>
-          <p>WEBINARS EXCLUSIVOS</p>
-          <H2>Menos Conversinha,</H2>
-          <H1>Mais Conversão</H1>
-          <p>Conheça as estratégias que <strong>mudaram o jogo</strong> e como aplicá-las no seu negócio</p>
-        </Branding>
-      </Header>
+        <Header>
+          <Image 
+              src={LogoImage} 
+              width='194px'
+              height='42px'
+              alt='logo leadster' 
+          />
+          <Branding>
+            <p>WEBINARS EXCLUSIVOS</p>
+            <H2>Menos Conversinha,</H2>
+            <H1>Mais Conversão</H1>
+            <p>Conheça as estratégias que <strong>mudaram o jogo</strong> e como aplicá-las no seu negócio</p>
+          </Branding>
+        </Header>
     </>
   )
 }
