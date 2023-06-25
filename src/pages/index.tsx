61fac148758da2e2eed72f0b07d2ea79f6094b81
iuriluna <iurideluna@gmail.com>
2023-06-25 11:33:55 -0300

    feat: criação e estilização da área de CTA

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,20 @@ import {
   ButtonHeader,
   BorderBranding,
   TextPromise,
-  StyleH1
+  StyleH1,
+  Proposal,
+  CTA_Proposal,
+  Question_CTA_Proposal,
+  Text_Proposal,
+  Border_Proposal,
+  ButtonSet_Proposal,
+  ObjectionBreak,
+  Button_Proposal,
+  NoCardText_Proposal,
+  Bar_Proposal,
+  RatingText_Proposal,
+
+  Div
 } from '../components/home'
 
 import Cards from '../components/cards'
@@ -16,6 +29,12 @@ import Cards from '../components/cards'
 import Image from 'next/image'
 import LogoImage from '../assets/logo.png'
 import IconHeader from '../assets/asset-header.png'
+import Comparative from '../assets/comparativo_img_CTA.png'
+import RdStation from '../assets/selo_RD.png'
+import NoCard from '../assets/no-card-dark.webp'
+import RatingImage from '../assets/rating.webp'
+
+
 
 export default function Home() {
   return (
@@ -32,16 +51,60 @@ export default function Home() {
             <H2>Menos Conversinha,</H2>
             <StyleH1>
               <H1>Mais Conversão</H1> <Image src={IconHeader} 
-              width='40px'
-              height='25px'
+              width='49px'
+              height='32px'
               alt='icon header' />
             </StyleH1>
             <BorderBranding></BorderBranding>
             <TextPromise>Conheça as estratégias que <strong>mudaram o jogo</strong> e como aplicá-las no seu negócio</TextPromise>
           </Branding>
-          
-          
         </Header>
+
+        <Div></Div>
+
+        <Proposal>
+          <Image src={Comparative} 
+              width='400px !important'
+              height='400px !important'
+              alt='Comparative Leadster' />
+
+            <CTA_Proposal>
+                <Question_CTA_Proposal>
+                  Pronto para triplificar sua <br></br>
+                  <strong>Geração de Leads?</strong>
+                </Question_CTA_Proposal>
+
+                <Text_Proposal>
+                    Criação e ativação em <strong>4 minutos.</strong>
+                </Text_Proposal>
+                <Border_Proposal></Border_Proposal>
+              
+
+              <ButtonSet_Proposal>
+                <Button_Proposal>VER DEMONSTRAÇÃO</Button_Proposal>
+                <Image src={RdStation} 
+                width='180px'
+                height='64px'
+                alt='RD Station' />
+              </ButtonSet_Proposal>
+
+              <ObjectionBreak>
+                <Image src={NoCard} 
+                width='16px'
+                height='16px'
+                alt='No-card' />
+                <NoCardText_Proposal><strong>Não</strong> é necessário Cartão de Crédito</NoCardText_Proposal>
+                <Bar_Proposal></Bar_Proposal>
+                <Image src={RatingImage} 
+                width='92px'
+                height='16px'
+                alt='Rating' />
+                <RatingText_Proposal>
+                  4.9/5 média de satisfação
+                </RatingText_Proposal>
+              </ObjectionBreak>
+            </CTA_Proposal>
+        </Proposal>
     </>
   )
 }
