6a2cbb0d0c10ae6c68d79bfb566bbd0ba3ca2354
iuriluna <iurideluna@gmail.com>
2023-06-26 13:07:02 -0300

    feat: finalização do footer

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -74,6 +74,8 @@ const Proposal = styled.section `
   display: flex;
   justify-content: space-evenly;
   align-items: center;
+
+  margin-bottom: 20px;
 `
 
 const CTA_Proposal = styled.div `
@@ -152,6 +154,14 @@ const Slogan_Footer = styled.p `
   font-size: 1em;
   margin: 8px;
 `
+
+const ChanelsLeadster_Footer = styled.div`
+  display: flex;
+    justify-content: space-evenly;
+    width: 100%;
+    margin-top: 20px;
+`
+
 const Links_Footer = styled.div `
     display: flex;
     flex-direction: column;
@@ -161,12 +171,12 @@ const PrincipalsLinks_Footer = styled.p `
 `
 const Link_Footer = styled.a `
   margin-bottom: 12px;
-  color: #A8BAC7;
+  color: #677294;
   cursor: pointer;
   transition: all 0.5s;
 
   &:hover {
-    color: black;
+    color: #00aff0;
   }
 `
 
@@ -191,9 +201,138 @@ const TitleMaterials_Footer = styled.p `
 
 `
 
+const ContactsLeadster_Footer =styled.div `
+  display: flex;
+  flex-direction: column;
+`
+
+const TitleSocialMedia_Footer = styled.p `
+
+
+`
+
 const Div = styled.div `
 
   height: 45vh;
+`
+
+const LinksSocialMedia_Footer = styled.div `
+    display: flex;
+    gap: 15px;
+    margin-bottom: 27px;
+`
+
+const AnchorFacebook = styled.a `
+    width: 46px;
+    height: 46px;
+
+    border-radius: 50%;
+    font-size: 14px;
+    line-height: 45px;
+    text-align: center;
+    transition: all .2s linear;
+    cursor: pointer;
+
+    color: #858da8;
+    background: #ebeef5;
+    
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    &:hover {
+    background: #00aff0;
+    color: #fff;
+    }
+`
+
+const FacebookIcon = styled.svg `
+
+`
+
+const AnchorInstagram = styled.a `
+    width: 46px;
+    height: 46px;
+
+    border-radius: 50%;
+    font-size: 14px;
+    line-height: 45px;
+    text-align: center;
+    transition: all .2s linear;
+    cursor: pointer;
+
+    color: #858da8;
+    background: #ebeef5;
+    
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    &:hover {
+    background: #00aff0;
+    color: #fff;
+    }
+`
+
+const InstagramIcon = styled.svg `
+
+`
+const AnchorLinkedin = styled.a `
+    width: 46px;
+    height: 46px;
+
+    border-radius: 50%;
+    font-size: 14px;
+    line-height: 45px;
+    text-align: center;
+    transition: all .2s linear;
+    cursor: pointer;
+
+    color: #858da8;
+    background: #ebeef5;
+    
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    &:hover {
+    background: #00aff0;
+    color: #fff;
+    }
+`
+
+
+const LinkedInIcon = styled.svg `
+
+`
+
+const Border_Footer = styled.div `
+    border-bottom: 1px solid #e9ecf3;
+    padding: 1px 0 55px;
+    width: 100%;
+`
+
+const Copyright_Footer = styled.p `
+  font-size: 0.9em;
+
+`
+const OfficialSiteLeadster_Footer = styled.a `
+  text-decoration: none;
+  color: #007DFF !important;
+
+`
+const Additional = styled.div `
+    display: flex;
+    justify-content: space-evenly;
+    margin-top: 45px;
+    width: 100%;
+
+`
+const Address = styled.p `
+  font-size: 0.9em;
 `
 
 
@@ -204,6 +343,7 @@ const Div = styled.div `
 
 export { Header, Logo, Branding, H1, H2, ButtonHeader, BorderBranding, TextPromise, StyleH1, IconHeader, 
           Proposal, CTA_Proposal, Question_CTA_Proposal, Text_Proposal, Border_Proposal, ButtonSet_Proposal, Button_Proposal, 
-          ObjectionBreak, NoCardText_Proposal, Bar_Proposal, RatingText_Proposal, Footer, Slogan_Footer, Links_Footer, PrincipalsLinks_Footer,
-          Link_Footer, Cases_Footer, TitleCase_Footer, Materials_Footer, TitleMaterials_Footer, Div 
+          ObjectionBreak, NoCardText_Proposal, Bar_Proposal, RatingText_Proposal, Footer, Slogan_Footer, Links_Footer, ChanelsLeadster_Footer, PrincipalsLinks_Footer,
+          Link_Footer, Cases_Footer, TitleCase_Footer, Materials_Footer, TitleMaterials_Footer, ContactsLeadster_Footer, TitleSocialMedia_Footer, LinksSocialMedia_Footer, 
+          AnchorFacebook, FacebookIcon, AnchorInstagram, InstagramIcon, AnchorLinkedin, LinkedInIcon, Border_Footer, Copyright_Footer, OfficialSiteLeadster_Footer, Additional, Address,  Div 
 }
